12afb94b9cb1c7be2b3e62f6d318448518ace72e
Daria Eremitskaya <31545001+ridesver@users.noreply.github.com>
2018-02-26 14:33:06 +0300

    Update 0.0.1.cpp

--- 0.0.1.cpp
+++ 0.0.1.cpp
@@ -45,7 +45,7 @@ struct complex_t
 		return  stream >> op >> real>> op >> imag >> op;
 	}
 
-	ostream & write(ostream & stream) 
+	ostream & write(ostream & stream) const
 	{
 		return stream  << "(" << real << ", " << imag << ")";
 	}
